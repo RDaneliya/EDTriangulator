65ee98705cd683868dab1c474e7abfee3eddfc0f
BANTYC <41577858+BANTYC@users.noreply.github.com>
2020-10-02 14:27:46 +0700

    Splitted triangulate file into two files Parallel queries instead of chained

--- js/tirangulate.js
+++ js/tirangulate.js
@@ -14,73 +14,6 @@ const candidates = document.getElementById('candidates');
 
 const edsmUrl = new URL('https://www.edsm.net/api-v1/sphere-systems');
 
-const loadSystems = (params) => {
-  const fetchUrl = `${edsmUrl}?systemName=${params.systemName}&minRadius=${params.minRadius}&radius=${params.radius}`;
-  return fetch(fetchUrl)
-    .then(response => {
-      return response.json();
-    })
-    .then(data => {
-      const withoutDist = cutField(data, 'distance');
-      return cutField(withoutDist, 'bodyCount');
-    });
-};
-
-const getSysCandidates = async(sysParams) => {
-  const systemsCandidates = [];
-  for(const i of sysParams) {
-    await loadSystems(i).then(systems => systemsCandidates.push(systems));
-  }
-  return systemsCandidates;
-};
-
-const cutField = (data, fieldName) => {
-  if(data[0].hasOwnProperty(`${fieldName}`)) {
-    const dataWithoutField = [];
-    for(let i of data) {
-      delete i[fieldName];
-      dataWithoutField.push(i);
-    }
-    return dataWithoutField;
-  }
-  return data;
-};
-
-const intersect = (arrayOfArrays) => {
-  const length = arrayOfArrays.length;
-
-  const freqMap = new Map();
-  for(const i of arrayOfArrays) {
-    for(const j of i) {
-      if(freqMap.has(j.name)) {
-        const currentValue = freqMap.get(j.name);
-        freqMap.set(j.name, currentValue + 1);
-      } else {
-        freqMap.set(j.name, 1);
-      }
-    }
-  }
-
-  const result = [];
-
-  for(const entry of freqMap) {
-    if(entry[1] === length) {
-      result.push(entry[0]);
-    }
-  }
-  return result;
-};
-
-const showArray = (dataArray, ul) => {
-  dataArray.forEach(item => {
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(item));
-    li.setAttribute('class', 'list-group-item');
-
-    ul.innerHTML = '';
-    ul.appendChild(li);
-  });
-};
 
 const submitForm = () => {
   event.preventDefault();
@@ -94,11 +27,6 @@ const submitForm = () => {
     params.push(sysParam);
   }
 
-  getSysCandidates(params)
-    .then(systemsLists => {
-      const result = intersect(systemsLists);
-      showArray(result, candidates);
-    });
-  return true;
+  getSysCandidates(params, candidates);
 };
 
